9d0a3bae7b0f798fa3261f7fa13b4a5810ef4672
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-08-31 20:15:13 +0530

    added users-component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,11 @@
 import { Appbar } from "../components/AppBar";
+import { Balance } from "../components/Balance";
 
 export const Dashboard = () => {
   return (
     <div>
       <Appbar />
+      <Balance value={4000} />
     </div>
   );
 };
