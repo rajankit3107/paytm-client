eb55f2e866a4788990680883d7a980473034afdd
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-08-31 19:13:30 +0530

    added heading component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Heading } from "./components/Heading";
+
+function App() {
+  return (
+    <>
+      <Heading label="Sign up" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Heading.tsx
@@ -0,0 +1,7 @@
+interface HeadingProps {
+  label: string;
+}
+
+export function Heading({ label }: HeadingProps) {
+  return <div className="font-bold text-4xl pt-6">{label}</div>;
+}
