049fe9bdbe4e6c8ce06b7e86de56adb363059cdc
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-08-31 20:10:16 +0530

    added appbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Singin";
+import { Dashboard } from "./pages/Dashboard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { Appbar } from "../components/AppBar";
+
+export const Dashboard = () => {
+  return (
+    <div>
+      <Appbar />
+    </div>
+  );
+};
