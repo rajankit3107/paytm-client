bf00edf1a769fcea06cf15acea9ef033a3717ba6
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-08-31 19:29:17 +0530

    comp : added subheading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Heading } from "./components/Heading";
+import { SubHeading } from "./components/SubHeading";
 
 function App() {
   return (
     <>
       <Heading label="Sign up" />
+      <SubHeading label="Enter your information to create an account" />
     </>
   );
 }
